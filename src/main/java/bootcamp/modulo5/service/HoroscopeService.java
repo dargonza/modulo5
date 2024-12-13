c631b342f6a911b6c3a2096b4a65a838a80c730f
dargonza <dario.gonzalez@outlook.com>
2024-12-13 01:50:17 -0300

    Implemento getChineseZodiacAnimal en HoroscopeService

---
+++ src/main/java/bootcamp/modulo5/service/HoroscopeService.java
@@ -0,0 +1,8 @@
+package bootcamp.modulo5.service;
+
+import java.time.LocalDate;
+
+public interface HoroscopeService {
+
+    String getChineseZodiacAnimal(LocalDate birthDate);
+}
---
+++ src/main/java/bootcamp/modulo5/service/HoroscopeServiceImpl.java
@@ -0,0 +1,40 @@
+package bootcamp.modulo5.service;
+
+import bootcamp.modulo5.model.Horoscope;
+import bootcamp.modulo5.repository.HoroscopeRepository;
+import bootcamp.modulo5.repository.HoroscopeRepositoryImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class HoroscopeServiceImpl implements HoroscopeService {
+
+    private final HoroscopeRepository horoscopeRepository;
+
+    public HoroscopeServiceImpl() {
+        this.horoscopeRepository = new HoroscopeRepositoryImpl();
+    }
+
+    @Override
+    public String getChineseZodiacAnimal(LocalDate birthDate) {
+
+        List<Horoscope> chineseZodiacSigns = horoscopeRepository.findAll();
+        Horoscope matchedZodiacSign = null;
+
+        for (Horoscope zodiacSign : chineseZodiacSigns) {
+            if (birthDate.isAfter(zodiacSign.getStartDate()) && birthDate.isBefore(zodiacSign.getEndDate())) {
+                matchedZodiacSign = zodiacSign;
+                break;
+            } else if (birthDate.isEqual(zodiacSign.getStartDate()) || birthDate.isEqual(zodiacSign.getEndDate())) {
+                matchedZodiacSign = zodiacSign;
+                break;
+            }
+        }
+
+        if (matchedZodiacSign == null) {
+            return null;
+        }
+
+        return matchedZodiacSign.getAnimal();
+    }
+}
